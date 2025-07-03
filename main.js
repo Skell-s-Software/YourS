e00f164972803c873ef8096853e42d3aa2d6bf10
skellent <skellent69@gmail.com>
2025-07-03 00:07:55 -0400

    Codigo base de servidor

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 /* Codigo del servidor */
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.static(`${__dirname}/public`));
+
+server.get("/", (requery, response) => {
+     response.sendFile(`${__dirname}/index.html`, (err) => {
+          if (err) {
+               console.log("Error: " + err.message);
+	       response.end(err.message);
+          }
+     });
+});
+
+server.listen('8000', '0.0.0.0', () => {
+     console.log("Servidor activo en: http://localhost:8000");
+});
